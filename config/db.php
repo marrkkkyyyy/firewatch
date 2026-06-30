8830d687d4889ce88ebff440aa98cfa10ff26f5f
Marrkkkyyyy <johnmarknoleal961@gmail.com>
2026-06-30 15:24:51 +0800

    Update db.php

--- config/db.php
+++ config/db.php
@@ -3,12 +3,17 @@
 $host = getenv('MYSQLHOST') ?: '127.0.0.1';
 $user = getenv('MYSQLUSER') ?: 'root';
 $pass = getenv('MYSQLPASSWORD') ?: '';
-$db   = getenv('MYSQLDATABASE') ?: 'firewatch_db1'; // Changed fallback from firewatch_db1 to railway
+$db   = getenv('MYSQLDATABASE') ?: 'firewatch_db1';
 $port = getenv('MYSQLPORT') ?: '3306';
 
 $conn = new mysqli($host, $user, $pass, $db, $port);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
+}
+
+function getDB() {
+    global $conn;
+    return $conn;
 }
 ?>
